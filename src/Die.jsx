32301caf6e91702173622162bbdafa0f9c17c996
unknown <pc@Lnedator2.pythagoras.se>
2024-01-21 15:46:14 +0100

    change color scheme

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Die(props) {
 
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "#BAC600" : "white"
     }
 
     return (
